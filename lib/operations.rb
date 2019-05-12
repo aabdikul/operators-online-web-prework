043c52127b953cb42b429108be59d2144feb973d
Aida Abdikulova <abdikulova@gmail.com>
2019-05-12 19:43:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,8 +10,8 @@ def unsafe?(speed)
 	end
 end
 
-
-
 def not_safe?(speed)
-	speed<40 ? "true" : "false"
+	speed < 40 ? true : false
+	speed > 60 ? true : false 
+	speed.between?(40,60) ? true : false
 end
